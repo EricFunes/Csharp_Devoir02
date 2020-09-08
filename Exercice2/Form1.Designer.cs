e91951675c615dba4e867d566e25987dc4a2dae5
Eric Funes-Figueroa <eric_funes_figueroa@hotmail.com>
2020-09-08 00:18:57 -0400

    Exercise 2 done.

--- Exercice2/Form1.Designer.cs
+++ Exercice2/Form1.Designer.cs
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.lblPalArr = new System.Windows.Forms.Label();
+            this.lblPalForEach = new System.Windows.Forms.Label();
+            this.lblPalFor = new System.Windows.Forms.Label();
+            this.txtPalindrome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(192, 25);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(305, 61);
+            this.btnValidate.TabIndex = 0;
+            this.btnValidate.Text = "Est-ce un palindrome?";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Location = new System.Drawing.Point(32, 153);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(48, 25);
+            this.lblWord.TabIndex = 1;
+            this.lblWord.Text = "Mot";
+            // 
+            // lblPalArr
+            // 
+            this.lblPalArr.AutoSize = true;
+            this.lblPalArr.Location = new System.Drawing.Point(32, 267);
+            this.lblPalArr.Name = "lblPalArr";
+            this.lblPalArr.Size = new System.Drawing.Size(318, 25);
+            this.lblPalArr.TabIndex = 2;
+            this.lblPalArr.Text = "Mot inversé par Array.Reverse()";
+            // 
+            // lblPalForEach
+            // 
+            this.lblPalForEach.AutoSize = true;
+            this.lblPalForEach.Location = new System.Drawing.Point(32, 332);
+            this.lblPalForEach.Name = "lblPalForEach";
+            this.lblPalForEach.Size = new System.Drawing.Size(284, 25);
+            this.lblPalForEach.TabIndex = 3;
+            this.lblPalForEach.Text = "Mot inversé avec un foreach";
+            // 
+            // lblPalFor
+            // 
+            this.lblPalFor.AutoSize = true;
+            this.lblPalFor.Location = new System.Drawing.Point(32, 397);
+            this.lblPalFor.Name = "lblPalFor";
+            this.lblPalFor.Size = new System.Drawing.Size(237, 25);
+            this.lblPalFor.TabIndex = 4;
+            this.lblPalFor.Text = "Mot inversé avec un for";
+            // 
+            // txtPalindrome
+            // 
+            this.txtPalindrome.Location = new System.Drawing.Point(192, 150);
+            this.txtPalindrome.Name = "txtPalindrome";
+            this.txtPalindrome.Size = new System.Drawing.Size(305, 31);
+            this.txtPalindrome.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(652, 450);
+            this.Controls.Add(this.txtPalindrome);
+            this.Controls.Add(this.lblPalFor);
+            this.Controls.Add(this.lblPalForEach);
+            this.Controls.Add(this.lblPalArr);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.btnValidate);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label lblPalArr;
+        private System.Windows.Forms.Label lblPalForEach;
+        private System.Windows.Forms.Label lblPalFor;
+        private System.Windows.Forms.TextBox txtPalindrome;
     }
 }
 
--- Exercice2/Form1.cs
+++ Exercice2/Form1.cs
@@ -16,5 +16,88 @@ namespace Exercice2
         {
             InitializeComponent();
         }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            isPalindromeArr();
+            isPalindromeForEach();
+            isPalindromeFor();
+        }
+        private void isPalindromeArr()
+        {
+            string palindrome = txtPalindrome.Text;
+            char[] array = palindrome.ToCharArray();
+            string first = palindrome.Substring(0, palindrome.Length / 2);
+
+            Array.Reverse(array);
+
+            string temp = new string(array);
+            string second = temp.Substring(0, temp.Length / 2);
+
+            if (first.Equals(second))
+            {
+                lblPalArr.Text = $"{palindrome} est un palindrome.";
+            }
+            else
+            {
+                lblPalArr.Text = $"{palindrome} n'est pas un palindrome.";
+            }
+        }
+
+        private void isPalindromeForEach()
+        {
+            string palindrome = txtPalindrome.Text;
+            char[] array = palindrome.ToCharArray();
+            Array.Reverse(array);
+            string temp = "";
+
+            foreach (char letter in array)
+            {
+                temp += letter;
+            }
+
+            if(validatePalindrome(palindrome, temp))
+            {
+                lblPalForEach.Text = $"{palindrome} est un palindrome.";
+            }
+            else
+            {
+                lblPalForEach.Text = $"{palindrome} n'est pas un palindrome.";
+            }
+        }
+
+        private void isPalindromeFor()
+        {
+            string palindrome = txtPalindrome.Text;
+            char[] array = palindrome.ToCharArray();
+            Array.Reverse(array);
+            string temp = "";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                temp += array[i];
+            }
+
+            if (validatePalindrome(palindrome, temp))
+            {
+                lblPalFor.Text = $"{palindrome} est un palindrome.";
+            }
+            else
+            {
+                lblPalFor.Text = $"{palindrome} n'est pas un palindrome.";
+            }
+        }
+        private Boolean validatePalindrome(string palindrome, string temp)
+        {
+            if (temp.Equals(palindrome))
+            {
+                MessageBox.Show("Oui");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
